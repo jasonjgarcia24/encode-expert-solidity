5939675dd6004c40630623fbfb050acb6dcffad8
jjgarcia.eth <jason.garcia24@gmail.com>
2023-05-06 22:49:50 -0500

    Added hw01 profile.

--- test/01-week/Level_0.t.sol
+++ test/01-week/Level_0.t.sol
@@ -1,5 +1,5 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity 0.8.17;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
 
 // import "forge-std/Test.sol";
 // import "../src/01_week/Level_0_00.sol";

f38fbd472819152e76003e8cbe0f54dd598d1032
jjgarcia.eth <jason.garcia24@gmail.com>
2023-05-06 22:35:32 -0500

    Added weekly foundry profiles.

--- test/01-week/Homework02.t.sol
+++ test/01-week/Homework02.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/01_week/hw/Homework02.sol";
+import "../../01-week/hw/Homework02.sol";
 
 contract CounterTest is Test {
     Homework02_01 public hw02_01;
--- test/02-week/Homework06.t.sol
+++ test/02-week/Homework06.t.sol
@@ -1,5 +1,5 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.17;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
 
 // import "forge-std/Test.sol";
 // import {Intro} from "../src/02_week/assembly/Assembly_1.sol";
